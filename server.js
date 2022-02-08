aa48d29d1348e2c5767ac4a6f2cfccdbe5eb86ce
marianaceci <marianaceciliaschmidt@gmail.com>
2022-02-08 11:37:36 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//https://nodejs.org/dist/latest-v16.x/docs/api/
+
+const http = require('http');
+const host = 'http://localhost';
+const port = 3000;
+const stats = require('./pcRamUsage');
+
+http.createServer((req, res) => {
+    let url = req.url;
+
+    if(url === '/stats') {
+        res.end(JSON.stringify(stats, null, 2));
+    } else {
+        res.end('<h1>Seja bem vinde</h1>');
+    }
+})
+.listen(port, () => console.log(`Server is running in ${host}:${port}, ${stats}`));
